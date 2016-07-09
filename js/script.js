a9c8fd1e99558b6b8a9c8e2e670f3059cefd3fb6
Vince Gonzales <allen_gonzales@dlsu.edu.ph>
2016-07-09 17:08:50 +0800

    updated criteria and colors

--- js/script.js
+++ js/script.js
@@ -31,6 +31,38 @@ $(document).ready(function()
 			}
 		});
 	};
+    
+    var colourizeGrading = function(nrOfRatings) {
+		$("#grading li a").each(function() {
+			if($(this).parent().index() <= nrOfRatings) {
+				$(this).stop().animate({ backgroundColor : "#" + colours[nrOfRatings] } , animationTime);
+			}
+		});
+	};
+    
+    var colourizeApproachability = function(nrOfRatings) {
+		$("#approachability li a").each(function() {
+			if($(this).parent().index() <= nrOfRatings) {
+				$(this).stop().animate({ backgroundColor : "#" + colours[nrOfRatings] } , animationTime);
+			}
+		});
+	};
+    
+    var colourizeLectures = function(nrOfRatings) {
+		$("#lectures li a").each(function() {
+			if($(this).parent().index() <= nrOfRatings) {
+				$(this).stop().animate({ backgroundColor : "#" + colours[nrOfRatings] } , animationTime);
+			}
+		});
+	};
+    
+    var colourizeExams = function(nrOfRatings) {
+		$("#exams li a").each(function() {
+			if($(this).parent().index() <= nrOfRatings) {
+				$(this).stop().animate({ backgroundColor : "#" + colours[nrOfRatings] } , animationTime);
+			}
+		});
+	};
 	
 	// Handle the hover events
 	$("#overall_rating li a").hover(function() {
@@ -76,6 +108,98 @@ $(document).ready(function()
 		// Restore all the rating to their original colours
 		$("#overall_difficulty li a").stop().animate({ backgroundColor : "#fff" } , animationTime);
 	});
+    
+    // Handle the hover events
+	$("#grading li a").hover(function() {
+		
+		// Empty the rating info box and fade in
+		ratingInfobox
+			.empty()
+			.stop()
+			.animate({ opacity : 1 }, animationTime);
+		
+		
+		// Call the colourize function with the given index
+		colourizeGrading($(this).parent().index());
+	}, function() {
+		
+		// Fade out the rating information box
+		ratingInfobox
+			.stop()
+			.animate({ opacity : 0 }, animationTime);
+		
+		// Restore all the rating to their original colours
+		$("#grading li a").stop().animate({ backgroundColor : "#fff" } , animationTime);
+	});
+    
+    // Handle the hover events
+	$("#approachability li a").hover(function() {
+		
+		// Empty the rating info box and fade in
+		ratingInfobox
+			.empty()
+			.stop()
+			.animate({ opacity : 1 }, animationTime);
+		
+		
+		// Call the colourize function with the given index
+		colourizeApproachability($(this).parent().index());
+	}, function() {
+		
+		// Fade out the rating information box
+		ratingInfobox
+			.stop()
+			.animate({ opacity : 0 }, animationTime);
+		
+		// Restore all the rating to their original colours
+		$("#approachability li a").stop().animate({ backgroundColor : "#fff" } , animationTime);
+	});
+    
+    // Handle the hover events
+	$("#lectures li a").hover(function() {
+		
+		// Empty the rating info box and fade in
+		ratingInfobox
+			.empty()
+			.stop()
+			.animate({ opacity : 1 }, animationTime);
+		
+		
+		// Call the colourize function with the given index
+		colourizeLectures($(this).parent().index());
+	}, function() {
+		
+		// Fade out the rating information box
+		ratingInfobox
+			.stop()
+			.animate({ opacity : 0 }, animationTime);
+		
+		// Restore all the rating to their original colours
+		$("#lectures li a").stop().animate({ backgroundColor : "#fff" } , animationTime);
+	});
+    
+    // Handle the hover events
+	$("#exams li a").hover(function() {
+		
+		// Empty the rating info box and fade in
+		ratingInfobox
+			.empty()
+			.stop()
+			.animate({ opacity : 1 }, animationTime);
+		
+		
+		// Call the colourize function with the given index
+		colourizeExams($(this).parent().index());
+	}, function() {
+		
+		// Fade out the rating information box
+		ratingInfobox
+			.stop()
+			.animate({ opacity : 0 }, animationTime);
+		
+		// Restore all the rating to their original colours
+		$("#exams li a").stop().animate({ backgroundColor : "#fff" } , animationTime);
+	});
 	
 	// Prevent the click event and show the rating
 	$("#overall_rating li a").click(function(e) {
@@ -83,6 +207,22 @@ $(document).ready(function()
 		alert("You voted on item number " + ($(this).parent().index() + 1));
 	});
     $("#overall_difficulty li a").click(function(e) {
+		e.preventDefault();
+		alert("You voted on item number " + ($(this).parent().index() + 1));
+	});
+    $("#lectures li a").click(function(e) {
+		e.preventDefault();
+		alert("You voted on item number " + ($(this).parent().index() + 1));
+	});
+    $("#approachability li a").click(function(e) {
+		e.preventDefault();
+		alert("You voted on item number " + ($(this).parent().index() + 1));
+	});
+    $("#grading li a").click(function(e) {
+		e.preventDefault();
+		alert("You voted on item number " + ($(this).parent().index() + 1));
+	});
+    $("#Exams li a").click(function(e) {
 		e.preventDefault();
 		alert("You voted on item number " + ($(this).parent().index() + 1));
 	});
